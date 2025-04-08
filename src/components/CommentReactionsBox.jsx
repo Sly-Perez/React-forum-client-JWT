748bd9e4ad5511a329e8e1095867322416d3e676
Sly-Perez <elaipereza@gmail.com>
2025-04-08 16:46:56 -0500

    src folder updated: components folder updated => CommentReactionsBox.jsx updated (Spinner component added to be displayed while we fetch the data from the API)

--- src/components/CommentReactionsBox.jsx
+++ src/components/CommentReactionsBox.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import './CommentReactionsBox.css';
 import { ApiDomain } from '../data/ApiDomain';
+import Spinner from './Spinner';
 
 const CommentReactionsBox = ({comment, handleRefreshOfComments}) => {
 
     const [existingCommentReaction, setExistingCommentReaction] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const token = localStorage.getItem("sessionToken") || "";
 
@@ -26,9 +29,13 @@ const CommentReactionsBox = ({comment, handleRefreshOfComments}) => {
 
             if(data.status === 200){
                 setExistingCommentReaction(response);
+                setIsLoading(false);
                 return;
             }
-            
+
+            if(data.status === 404){
+                setIsLoading(false);
+            }
         } 
         catch (error) {
             //navigate("/serverError");
@@ -53,6 +60,7 @@ const CommentReactionsBox = ({comment, handleRefreshOfComments}) => {
             newReactionValue = 1;
         }
         
+        setIsLoading(true);
         
         const formData = new FormData();
         if(reactionName === "isLike"){
@@ -92,24 +100,32 @@ const CommentReactionsBox = ({comment, handleRefreshOfComments}) => {
 
     return (
         <>
-            <div className="d-flex flex-row">                  
-                <i className={`${existingCommentReaction.isLike && existingCommentReaction.isLike === 1 ? "fa-solid" : "fa-regular"} fa-thumbs-up f-black-color like-dislike-icon mx-10 cursor-pointer`}
-                    onClick={(event)=>reactToComment(event, comment, "isLike")}
-                ></i>
+            {
+                isLoading
+                ?
+                < Spinner sizeLevel={1} />
+                :
+                <>
+                    <div className="d-flex flex-row">                  
+                        <i className={`${existingCommentReaction.isLike && existingCommentReaction.isLike === 1 ? "fa-solid" : "fa-regular"} fa-thumbs-up f-black-color like-dislike-icon mx-10 cursor-pointer`}
+                            onClick={(event)=>reactToComment(event, comment, "isLike")}
+                        ></i>
 
-                <p className="little-gray-text">
-                    {comment.likesQuantity}
-                </p>
-            </div>
-            <div className="d-flex flex-row">
-                <i className={`${existingCommentReaction.isDislike && existingCommentReaction.isDislike === 1 ? "fa-solid" : "fa-regular"} fa-thumbs-down f-black-color like-dislike-icon mx-10 cursor-pointer`}
-                    onClick={(event)=>reactToComment(event, comment, "isDislike")}
-                ></i>
-                
-                <p className="little-gray-text">
-                    {comment.dislikesQuantity}
-                </p>
-            </div>
+                        <p className="little-gray-text">
+                            {comment.likesQuantity}
+                        </p>
+                    </div>
+                    <div className="d-flex flex-row">
+                        <i className={`${existingCommentReaction.isDislike && existingCommentReaction.isDislike === 1 ? "fa-solid" : "fa-regular"} fa-thumbs-down f-black-color like-dislike-icon mx-10 cursor-pointer`}
+                            onClick={(event)=>reactToComment(event, comment, "isDislike")}
+                        ></i>
+                        
+                        <p className="little-gray-text">
+                            {comment.dislikesQuantity}
+                        </p>
+                    </div>
+                </>
+            }
         </>
     )
 }
