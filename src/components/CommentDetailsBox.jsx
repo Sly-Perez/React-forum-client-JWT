bce321a5c80f767eb517ce0275e5fb61ba7d63da
Sly-Perez <elaipereza@gmail.com>
2025-04-08 16:45:11 -0500

    src folder updated: components folder updated => CommentDetailsBox.jsx updated (Spinner component added to be displayed while we fetch the data from the API)

--- src/components/CommentDetailsBox.jsx
+++ src/components/CommentDetailsBox.jsx
@@ -4,12 +4,15 @@ import AddCommentForm from "./AddCommentForm";
 import { ApiDomain } from "../data/ApiDomain";
 import RepliesListing from "./RepliesListing";
 import CommentReactionsBox from "./CommentReactionsBox";
+import Spinner from "./Spinner";
 
 const CommentDetailsBox = ({userInSession, comment, userId, handleRefreshOfComments}) => {
 
     const [user, setUser] = useState([]);
     const [userPicture, setUserPicture] = useState(null);
     const [commentImages, setCommentImages] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const [addCommentIsShown, setAddCommentIsShown] = useState(false);
 
@@ -96,6 +99,7 @@ const CommentDetailsBox = ({userInSession, comment, userId, handleRefreshOfComme
                 const blob = await data.blob();
                 const url = URL.createObjectURL(blob);
                 setUserPicture(url);
+                setIsLoading(false);
                 return;
             }
         }
@@ -139,81 +143,93 @@ const CommentDetailsBox = ({userInSession, comment, userId, handleRefreshOfComme
         <>
             <div className="post-details-comment px-20 py-20 pb-10">
                 <div className="d-flex flex-row gap-10 mb-5">
-                    <div className="">
-                        <Link to={`/users/profile/${user.userId}`}>
-                            <img className="img-fluid icon-sized-img circle-like-border" src={userPicture} alt={`${user.username}'s profile picture`} />
-                        </Link>
-                    </div>
-                    <div className="w-80-percent">
-                        <div className="d-flex flex-row align-items-center gap-10">
-                            <p>
-                                <b>
-                                    <Link className="black-text" to={`/users/profile/${user.userId}`}>
-                                        {user.username}
-                                    </Link>
-                                </b>
-                            </p>
-                            <span className="little-gray-text">
-                                {comment.publishDatetime}
-                            </span>
-                        </div>
-                        <div className="word-wrap-break">
-                            <p className="my-10">
-                                {comment.description}
-                            </p>
-                            
-                            {
-                                comment.numberOfImages > 0
-                                ?
-                                <div className="post-details-comment-img per-row-3-col align-items-center gap-10 mx-10 my-10">
+                    {
+                        isLoading
+                        ?
+                        < Spinner />
+                        :
+                        <>
+                            <div className="">
+                                <Link to={`/users/profile/${user.userId}`}>
+                                    <img className="img-fluid icon-sized-img circle-like-border" src={userPicture} alt={`${user.username}'s profile picture`} />
+                                </Link>
+                            </div>
+                            <div className="w-80-percent">
+                                <div className="d-flex flex-row align-items-center gap-10">
+                                    <p>
+                                        <b>
+                                            <Link className="black-text" to={`/users/profile/${user.userId}`}>
+                                                {user.username}
+                                            </Link>
+                                        </b>
+                                    </p>
+                                    <span className="little-gray-text">
+                                        {comment.publishDatetime}
+                                    </span>
+                                </div>
+                                <div className="word-wrap-break">
+                                    <p className="my-10">
+                                        {comment.description}
+                                    </p>
+                                    
                                     {
-                                        commentImages.map((item, index)=>(
-                                            <img className="img-fluid mx-10" src={item} alt={`${user.username}'s comment picture`} key={index} />
-                                        ))
+                                        comment.numberOfImages > 0
+                                        ?
+                                        <div className="post-details-comment-img per-row-3-col align-items-center gap-10 mx-10 my-10">
+                                            {
+                                                commentImages.map((item, index)=>(
+                                                    <img className="img-fluid mx-10" src={item} alt={`${user.username}'s comment picture`} key={index} />
+                                                ))
+                                            }
+                                        </div>
+                                        :
+                                        null
                                     }
+                                    
+                                    
+                                    <div className="d-flex flex-row align-items-center gap-10">
+
+                                        < CommentReactionsBox comment={comment} handleRefreshOfComments={handleRefreshOfComments} />
+
+                                        <div className="reply-to-comment mx-10">
+                                            <Link className="little-gray-text like-dislike-icon" 
+                                                onClick={(event)=>toggleAddCommentState(event)}
+                                            >
+                                                Reply
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    < AddCommentForm addCommentIsShown={addCommentIsShown} hideAddCommentForm={hideAddCommentForm} 
+                                        postId={comment.postId} responseTo={comment.commentId} onCommentSubmit={() => handleRefreshOfComments()}
+                                    />
+                                    
+                                </div>
+                            </div>
+
+                            {
+                                (userInSession.userId === user.userId) || (userInSession.hierarchyLevelId === 1)
+                                ?
+                                <div className="">
+                                    <i className="fa-solid fa-trash-can white-to-dark-red-icon" onClick={()=>deleteComment(comment.commentId)}></i>
                                 </div>
                                 :
                                 null
                             }
-                            
-                            
-                            <div className="d-flex flex-row align-items-center gap-10">
-
-                                < CommentReactionsBox comment={comment} handleRefreshOfComments={handleRefreshOfComments} />
-
-                                <div className="reply-to-comment mx-10">
-                                    <Link className="little-gray-text like-dislike-icon" 
-                                        onClick={(event)=>toggleAddCommentState(event)}
-                                    >
-                                        Reply
-                                    </Link>
-                                </div>
-                            </div>
-                            < AddCommentForm addCommentIsShown={addCommentIsShown} hideAddCommentForm={hideAddCommentForm} 
-                                postId={comment.postId} responseTo={comment.commentId} onCommentSubmit={() => handleRefreshOfComments()}
-                            />
-                            
-                        </div>
-                    </div>
-
-                    {
-                        (userInSession.userId === user.userId) || (userInSession.hierarchyLevelId === 1)
-                        ?
-                        <div className="">
-                            <i className="fa-solid fa-trash-can white-to-dark-red-icon" onClick={()=>deleteComment(comment.commentId)}></i>
-                        </div>
-                        :
-                        null
+                        </>
                     }
                 </div>
             </div>
 
             {
-                comment.repliesQuantity > 0
+                isLoading
                 ?
-                < RepliesListing userInSession={userInSession} comment={comment} handleRefreshOfComments={handleRefreshOfComments}/>
-                :
                 null
+                :
+                    comment.repliesQuantity > 0
+                    ?
+                    < RepliesListing userInSession={userInSession} comment={comment} handleRefreshOfComments={handleRefreshOfComments}/>
+                    :
+                    null
             }
         </>
     )
