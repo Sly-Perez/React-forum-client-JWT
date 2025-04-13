4275734e17618aca1d392e2f9e34806127474002
Sly-Perez <elaipereza@gmail.com>
2025-04-13 14:28:52 -0500

    src folder updated: components folder updated => AlternativeNavbar.jsx added (An alternative Navbar with an alternative design added)

---
+++ src/components/AlternativeNavbar.jsx
@@ -0,0 +1,179 @@
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import './MainNavbar.css';
+import WMIconNavBar from './../assets/images/page/WM-icon-nav-bar.webp';
+
+import { NavbarData } from '../data/NavbarData';
+
+import { useEffect, useRef, useState } from 'react';
+import { preventDefaultEvent } from '../utils/PreventDefaultEvent';
+import { ApiDomain } from '../data/ApiDomain';
+
+import Spinner from "./Spinner";
+
+const AlternativeNavbar = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [user, setUser] = useState([]);
+    const [userPicture, setUserPicture] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const token = localStorage.getItem("sessionToken") || "";
+    
+    //dropdown list state
+    const [dropdownIsActive, setDropdownIsActive] = useState(false);
+
+    const dropdownRef = useRef(null);
+
+    useEffect(()=>{
+        readService();
+        
+        document.addEventListener("click", handleClickOutside);
+
+        return ()=>{
+            document.removeEventListener("click", handleClickOutside);
+        }
+    }, []);
+
+    const readService = async()=>{
+        const apiUrl = `${ApiDomain}/users/my/profile`;
+        try{
+            const data = await fetch(apiUrl, {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+
+            const response = await data.json();
+
+            if(data.status === 200){
+                setUser(response);
+                readServicePictures(response.userId);
+                return;
+            }
+
+            if(data.status === 401){
+                navigate("/");
+                return;
+            }
+            
+        } 
+        catch (error) {
+            navigate("/serverError");
+            return;
+        }
+    }
+
+    const readServicePictures = async(id)=>{
+        const apiUrl = `${ApiDomain}/users/${id}/pictures`;
+        try{
+            const data = await fetch(apiUrl, {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+
+            if(data.status === 200){
+                const blob = await data.blob();
+                const url = URL.createObjectURL(blob);
+                setUserPicture(url);
+                setIsLoading(false);
+            }
+        }
+        catch (error) {
+            navigate("/serverError");
+            return;
+        }
+    }
+
+    const logout = async(event)=>{
+        preventDefaultEvent(event);
+
+        const apiUrl = `${ApiDomain}/users/logout`;
+        try {
+            
+            const data = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            
+            if(data.status === 200){
+                localStorage.removeItem("sessionToken");
+                navigate("/");
+                return;
+            }
+
+        } catch (error) {
+            navigate("/serverError");
+            return;
+        }
+    }
+
+    const toggleDropdownIsActive = (event)=>{
+        preventDefaultEvent(event);
+        setDropdownIsActive(!dropdownIsActive);
+    }
+
+    const handleClickOutside = (event)=>{
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target)){
+            setDropdownIsActive(false);
+        }
+    }
+
+    return (
+        <header>
+            <nav className="alternative-nav-bar py-20 mx-auto">
+                <div className="d-flex flex-row align-items-center justify-content-between px-20">
+                    <Link to="/weekieTalkie" className="wt-custom-animation">
+                        <div className="d-flex align-items-center">
+                            <span className="f-size-22 text-shadow-sm">WeekieMochi</span>
+                            <img className="img-fluid icon-sized-img png-border" src={WMIconNavBar} alt="Weekie Mochi icon: cat playing"/>
+                        </div>
+                    </Link>
+                    <ul className="d-flex px-20">
+                        {
+                            NavbarData.map((item, index)=>(
+                                <li className="mx-15" key={index}>
+                                    <Link to={item.url} title={item.title} className="wt-custom-animation">
+                                        {item.option}
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    {
+                        isLoading
+                        ?
+                        < Spinner sizeLevel={2}/>
+                        :
+                        <div className="dropdown-list" ref={dropdownRef}>
+                            <Link to="" className="d-flex flex-row align-items-center user-nav-bar-option wt-custom-animation mx-15 gap-10" onClick={(event)=>toggleDropdownIsActive(event)}>
+                                <span>{user.username}</span>
+                                <img className="img-fluid icon-sized-img circle-like-border" src={userPicture} alt={`${user.username}'s picture`}/>
+                            </Link>
+                            <ul className={`d-flex flex-column dropdown-list-content ${dropdownIsActive ? "" : "d-none"} gap-10 p-15`}>
+                                <li className="pt-5">
+                                    <Link to={`/users/profile/${user.userId}`} className="wt-custom-animation">
+                                        Profile
+                                    </Link>
+                                </li> 
+                                <li className="border-top-1 pt-5" onClick={(event)=>logout(event)}>
+                                    <Link to="" className="wt-custom-animation">
+                                        Log out
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    }
+
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default AlternativeNavbar;
