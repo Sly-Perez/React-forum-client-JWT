1d7c77b2219bbab93667534f9b9406ddfc817af4
Sly-Perez <elaipereza@gmail.com>
2025-07-19 15:42:21 -0500

    Implementation of verification email resend: Some changes were made to be able to resend the verification email to a recently created account. After signing up, if everything went okay, the user will see a box with the message 'Account created. Check email' (sent by the API). In case the user wants to resend the verification email, they will have a button below the message to resend the email as they prefer. Each time they press the button, the message sent by the API (response) will display properly. src > components > SignUpForm.jsx updated to implement the logic previously mentioned.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 import './SignUpForm.css';
 import { useRef, useState } from 'react';
 import { preventDefaultEvent } from '../utils/PreventDefaultEvent';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { ApiDomain } from '../data/ApiDomain';
 
@@ -9,7 +9,6 @@ import Spinner from './Spinner';
 import ImageCropper from './ImageCropper';
 
 const SignUpForm = () => {
-    const navigate = useNavigate();
     
     const [username, setUsername] = useState("");
     const [userEmail, setUserEmail] = useState("");
@@ -22,6 +21,13 @@ const SignUpForm = () => {
     const [errorsList, setErrorsList] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
+
+    const [hideForm, setHideForm] = useState(false);
+    const [successMessage, setSuccessMessage] = useState([]);
+
+    const [signedUpUserId, setSignedUpUserId] = useState(null);
+
+    const token = localStorage.getItem("sessionToken") || "";
 
     const submitForm = async(event)=>{
         preventDefaultEvent(event);
@@ -48,7 +54,9 @@ const SignUpForm = () => {
 
             setIsLoading(false);
             if(data.status === 201){
-                navigate('/', { state: { successMessage: "Account Created Successfully. Log In!" } });
+                setSuccessMessage(response.message);
+                setSignedUpUserId(response.user.userId);
+                setHideForm(true);
 
                 setUsername("");
                 setUserEmail("");
@@ -61,6 +69,45 @@ const SignUpForm = () => {
             setErrorsList(message);
             setUserPassword("");
             setUserPicture("");
+        } 
+        catch (error) {
+            setIsLoading(false);
+            console.error("Error uploading data:", error);
+        }
+    }
+
+    const resendConfirmationEmail = async()=>{
+        setIsLoading(true);
+        setSuccessMessage([]);
+        setErrorsList([]);
+
+        const apiUrl =  `${ApiDomain}/verifications/emailResends`;
+
+        const formData = new FormData();
+        formData.append("jsonBody", JSON.stringify({
+            userId: signedUpUserId
+        }));
+
+        try{
+            const data = await fetch(apiUrl, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                }
+            });
+            
+            const response = await data.json();
+
+            setIsLoading(false);
+
+            if(data.status == 200){
+                setSuccessMessage(response.message);
+                return;
+            }
+
+            setErrorsList(response.errors);
+            return;
         } 
         catch (error) {
             setIsLoading(false);
@@ -89,54 +136,89 @@ const SignUpForm = () => {
                 ?
                 < Spinner />
                 :
-                <form className="sign-up-form w-25-percent squared-border d-flex flex-column py-20 px-20 gap-10" action="" onSubmit={(event)=>submitForm(event)}>
-
+                <>
                     {
-                        (errorsList.length > 0)
+                        hideForm
                         ?
-                        <ul className="d-flex flex-column alert-box py-20 px-20 m-0">
-                            {
-                                errorsList.map((item, index)=>(
-                                    <li className="f-size-14 list-style-circle" key={index}>{item}</li>
-                                ))
+                        <div className="sign-up-form w-25-percent squared-border d-flex flex-column py-20 px-20 gap-10">
+                            { 
+                                (successMessage.length > 0)
+                                ?
+                                    <ul className="d-flex flex-column success-box py-20 px-20 m-0">
+                                        {
+                                            successMessage.map((item, index)=>(
+                                                <li className="f-size-14" key={index}>{item}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                :
+                                    (errorsList.length > 0)
+                                    ?
+                                        <ul className="d-flex flex-column alert-box py-20 px-20 m-0">
+                                            {
+                                                errorsList.map((item, index)=>(
+                                                    <li className="f-size-14" key={index}>{item}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                    :
+                                        null
                             }
-                        </ul>
-                        :
-                        null
-                    }
-
-                    <div className="d-flex flex-column gap-10">
-                        <h2>Username: </h2>
-                        <input type="text" minLength="1" maxLength="30" id="username" className="add-header-input" value={username} onChange={(event)=>setUsername(event.target.value)}/>
-                    </div>
-                    <div className="d-flex flex-column gap-10">
-                        <h2>Email: </h2>
-                        <input type="email" minLength="1" maxLength="100" id="userEmail" className="add-header-input" value={userEmail} onChange={(event)=>setUserEmail(event.target.value)}/>
-                    </div>
-                    <div className="d-flex flex-column gap-10">
-                        <h2>Password: </h2>
-                        <input type="password" minLength="1" maxLength="30" id="userPassword" className="add-header-input" value={userPassword} onChange={(event)=>setUserPassword(event.target.value)}/>
-                    </div>
-                    <div className="d-flex flex-column gap-10">
-                        <div className="">
-                            <h2>Profile Picture: </h2>
-                            <p>(optional)</p>
+                            <button className="d-flex flex-row gap-10 pagination-item cursor-pointer text-align-center px-20 py-10" onClick={()=>resendConfirmationEmail()}>
+                                <span>Resend email</span>
+                            </button>
                         </div>
-                        <input className="upload-file-input cursor-pointer w-fit-content mt-10 mb-10" type="file" 
-                            id="userPicture" 
-                            ref={fileInputRef}
-                            onChange={(event)=>handleImageUpload(event.target.files[0])}
-                            accept=".gif, .jpeg, .jpg, .png, .webp"
-                        />
-                        <p className="f-size-12">[In case of gifs, they need to have the same width as height]</p>
-                    </div>
-                    <div className="d-flex flex-column align-items-center gap-10 mt-10 mb-10">
-                        <button type="submit" className="w-100-percent pagination-item cursor-pointer px-20 py-10">Sign Up</button>
-                        <Link className="WT-anchor w-fit-content cursor-pointer" to="/">
-                            Have an account already?
-                        </Link>
-                    </div>
-                </form>
+                        :
+                        <form className="sign-up-form w-25-percent squared-border d-flex flex-column py-20 px-20 gap-10" action="" onSubmit={(event)=>submitForm(event)}>
+
+                            {
+                                (errorsList.length > 0)
+                                ?
+                                <ul className="d-flex flex-column alert-box py-20 px-20 m-0">
+                                    {
+                                        errorsList.map((item, index)=>(
+                                            <li className="f-size-14 list-style-circle" key={index}>{item}</li>
+                                        ))
+                                    }
+                                </ul>
+                                :
+                                null
+                            }
+
+                            <div className="d-flex flex-column gap-10">
+                                <h2>Username: </h2>
+                                <input type="text" minLength="1" maxLength="30" id="username" className="add-header-input" value={username} onChange={(event)=>setUsername(event.target.value)}/>
+                            </div>
+                            <div className="d-flex flex-column gap-10">
+                                <h2>Email: </h2>
+                                <input type="email" minLength="1" maxLength="100" id="userEmail" className="add-header-input" value={userEmail} onChange={(event)=>setUserEmail(event.target.value)}/>
+                            </div>
+                            <div className="d-flex flex-column gap-10">
+                                <h2>Password: </h2>
+                                <input type="password" minLength="1" maxLength="30" id="userPassword" className="add-header-input" value={userPassword} onChange={(event)=>setUserPassword(event.target.value)}/>
+                            </div>
+                            <div className="d-flex flex-column gap-10">
+                                <div className="">
+                                    <h2>Profile Picture: </h2>
+                                    <p>(optional)</p>
+                                </div>
+                                <input className="upload-file-input cursor-pointer w-fit-content mt-10 mb-10" type="file" 
+                                    id="userPicture" 
+                                    ref={fileInputRef}
+                                    onChange={(event)=>handleImageUpload(event.target.files[0])}
+                                    accept=".gif, .jpeg, .jpg, .png, .webp"
+                                />
+                                <p className="f-size-12">[In case of gifs, they need to have the same width as height]</p>
+                            </div>
+                            <div className="d-flex flex-column align-items-center gap-10 mt-10 mb-10">
+                                <button type="submit" className="w-100-percent pagination-item cursor-pointer px-20 py-10">Sign Up</button>
+                                <Link className="WT-anchor w-fit-content cursor-pointer" to="/">
+                                    Have an account already?
+                                </Link>
+                            </div>
+                        </form>
+                    }
+                </>
             }
 
             {
